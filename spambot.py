0c6ef353eadd56c53c316b861903e8cce69798e6
Dubz <wchadun@gmail.com>
2021-02-20 21:39:21 +0400

    Update spambot.py

--- spambot.py
+++ spambot.py
@@ -7,7 +7,7 @@ f = open(#"NameOfThe.TxtFile", 'r')
 # Read all lines of the .txt file and store in text variable
 text = f.readlines()
 # for loop with determined number of times each line should be spammed
-for _ in range(#enter number of times each line should be spammed as an int):
+for _ in range(#enter number of times each line should be spammed):
     # for each word in text, spam 
     for word in text:
         pyautogui.typewrite(word)
